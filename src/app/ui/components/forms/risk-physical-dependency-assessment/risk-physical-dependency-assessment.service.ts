930171f5d3c700a68751633bd927a5364adf07bf
Ishaan Kr. <ishaan.kumar@bharuwasolutions.com>
2024-05-01 15:09:18 +0530

    Risk - Physical Dependency Form Completed

---
+++ src/app/ui/components/forms/risk-physical-dependency-assessment/risk-physical-dependency-assessment.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+@Injectable({
+    providedIn: 'root',
+})
+
+export class RiskPhysicalDependencyAssessmentService {
+
+  constructor(private _httpclient: HttpClient) {}
+
+  GetRiskPhysicalDependencyAssessmentFormById(selectedFormID: string): Observable<any> {
+      const reqHeader = new HttpHeaders({
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': environment.BaseUriAdmin,
+          'Authorization': 'Bearer ' + localStorage.getItem('token')
+      });
+      let params = new HttpParams();
+      params = params.append('fromId', selectedFormID);
+      return this._httpclient.get<any>(
+          environment.BaseUriAdmin +
+              'api/Admin/GetRiskPhysicalDependencyAssessmentFormById',
+          { headers: reqHeader, params: params }
+      );
+  }
+
+  AddInsertUpdateFormData(formdata: any): Observable<any> {
+      let reqHeader = new HttpHeaders({
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': environment.BaseUriAdmin,
+          'Authorization': 'Bearer ' + localStorage.getItem('token')
+      });   
+      
+      console.log(formdata);
+
+      let params = new HttpParams();
+      var data = JSON.stringify(formdata).toString();
+      return this._httpclient.post<any>(
+          environment.BaseUriAdmin +
+              'api/Admin/InsertUpdateRiskPhysicalDependencyAssessmentForm',
+          data,
+          { headers: reqHeader, params: params }
+      );
+  }
+}
