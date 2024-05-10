2b7df4cf2e476bdadaaa14017608caecf5cf91d7
Mudit Madhwal <mudit.madhwal@bharuwasolutions.com>
2024-05-10 12:11:09 +0530

    Completed working of Healthcare Support Tool Form

---
+++ src/app/ui/components/forms/healthcare-support-tool/healthcare-support-tool.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HealthcareSupportToolService {
+
+  constructor(private _httpclient: HttpClient) { }
+
+  //#region HealtcareSupportTool Form
+
+  GetHealthcareSupportDetails(fromId: any) {
+    let reqHeader = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': environment.BaseUriAdmin,
+        'Authorization': 'Bearer ' + localStorage.getItem('token')
+    });
+    let params = new HttpParams();
+    params = params.append('fromId', fromId);
+    return this._httpclient.get<any>(
+        environment.BaseUriAdmin + 'api/Admin/GetHealthcareSupportForm',
+        { headers: reqHeader, params: params }
+    );
+}
+
+InsertUpdateHealthcareSupportForm(
+  HealthcareSupportFormsData: any
+): Observable<any> {
+    let reqHeader = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': environment.BaseUriAdmin,
+        'Authorization': 'Bearer ' + localStorage.getItem('token')
+    });
+    let params = new HttpParams();
+    var data = JSON.stringify(HealthcareSupportFormsData).toString();
+    console.log(data);
+    return this._httpclient.post<any>(
+        environment.BaseUriAdmin +
+            'api/Admin/InsertUpdateHealthcareSupportForm',
+        data,
+        { headers: reqHeader, params: params }
+    );
+}
+
+  //#endregion
+}
