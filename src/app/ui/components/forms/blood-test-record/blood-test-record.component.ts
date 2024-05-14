13eddc81f8cf52ca0c1d7808affccc549061c86b
Ishaan Kr. <ishaan.kumar@bharuwasolutions.com>
2024-05-14 10:23:56 +0530

    Bllood Test Record Form Completed

--- src/app/ui/components/forms/blood-test-record/blood-test-record.component.ts
+++ src/app/ui/components/forms/blood-test-record/blood-test-record.component.ts
@@ -1,15 +1,195 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+    Component,
+    EventEmitter,
+    Input,
+    OnInit,
+    Output,
+    SimpleChanges,
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, catchError, forkJoin, map, of } from 'rxjs';
+import { AppComponentBase } from 'src/app/app-component-base';
+import {
+    ConstantsService,   
+    CustomDateFormat,
+    FormTypes,
+} from 'src/app/ui/service/constants.service';
+import { MasterService } from 'src/app/ui/service/master.service';
+import { UtilityService } from 'src/app/utility/utility.service';
+import { BloodTestRecordService } from './blood-test-record.service';
 
 @Component({
-  selector: 'app-blood-test-record',
-  templateUrl: './blood-test-record.component.html',
-  styleUrls: ['./blood-test-record.component.scss']
+    selector: 'app-blood-test-record',
+    templateUrl: './blood-test-record.component.html',
+    styleUrls: ['./blood-test-record.component.scss'],
 })
-export class BloodTestRecordComponent implements OnInit {
-  @Input() preSelectedFormData: any=<any>{};
-  @Output() EmitUpdateForm: EventEmitter<any> = new EventEmitter<any>();
-  constructor() { }
-  ngOnInit(): void {
-  }
 
+export class BloodTestRecordComponent
+    extends AppComponentBase
+    implements OnInit
+{
+    @Input() preSelectedFormData: any = <any>{};
+    @Output() EmitUpdateForm: EventEmitter<any> = new EventEmitter<any>();
+
+    customDateFormat = CustomDateFormat;
+    BloodTestFormData: any = <any>{};
+
+    //Form which is selected to edit or view
+    isEditable: boolean;
+    //Need to be passed from form Dashboard
+    StatementType: string = null;
+
+    //Patient Details
+    userId: any;
+    residentAdmissionInfoId: any;
+
+    //CreatedBy or ModifiedBy
+    loginId: any;
+
+    constructor(
+        private _ConstantServices: ConstantsService,
+        private route: ActivatedRoute,
+        private _UtilityService: UtilityService,
+        private _MasterService: MasterService,
+        private _FormService: BloodTestRecordService
+    ) {
+        super();
+        this._ConstantServices.ActiveMenuName =
+            'Blood Test Record Form';
+        this.loginId = localStorage.getItem('userId');
+
+        this.unsubscribe.add = this.route.queryParams.subscribe((params) => {
+            var ParamsArray = this._ConstantServices.GetParmasVal(params['q']);
+
+            if (ParamsArray?.length > 0) {
+                this.userId =
+                    ParamsArray.find((e) => e.FieldStr == 'id')?.FieldVal ||
+                    null;
+                this.residentAdmissionInfoId =
+                    ParamsArray.find((e) => e.FieldStr == 'admissionid')
+                        ?.FieldVal || null;
+            }
+        });
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        this.isEditable = this.preSelectedFormData.isEditable;
+        if (this.preSelectedFormData.selectedFormID != null) {
+            this.BloodTestFormData = <any>{};
+            this.GetSelectedFormDetails(
+                this.preSelectedFormData.selectedFormID
+            );
+            this.StatementType = 'Update';
+        } else {
+            this.ResetModel();
+        }
+    }
+
+    ngOnInit(): void {
+
+        this.isEditable = this.preSelectedFormData.isEditable;
+
+        if (this.preSelectedFormData.selectedFormID != null) {
+            this.BloodTestFormData = <any>{};
+            this.GetSelectedFormDetails(
+                this.preSelectedFormData.selectedFormID
+            );
+
+            this.StatementType = 'Update';
+        } else {
+            this.ResetModel();
+        }
+    }
+
+    SaveAsPDF() {}
+
+    saveAsUnfinished() {
+        this.BloodTestFormData.IsFormCompleted = false;
+        this.Save();
+    }
+
+    completeForm() {
+        this.BloodTestFormData.IsFormCompleted = true;
+        this.Save();
+    }
+
+    GetSelectedFormDetails(formId: string) {
+        this._UtilityService.showSpinner();
+        this.unsubscribe.add = this._FormService
+            .GetBloodTestRecordFormById(formId)
+            .subscribe({
+                next: (data) => {
+                    this._UtilityService.hideSpinner();
+                    if (data.actionResult.success == true) {
+                        var tdata = JSON.parse(data.actionResult.result);
+                        tdata = tdata ? tdata : {};
+                        
+                        console.log(tdata);
+
+                        this.BloodTestFormData = tdata;
+                    } else {
+                        this.BloodTestFormData = {};
+                    }
+                },
+                error: (e) => {
+                    this._UtilityService.hideSpinner();
+                    this._UtilityService.showErrorAlert(e.message);
+                },
+            });
+    }
+
+    Save() {
+        if (
+            this.userId != null &&
+            this.residentAdmissionInfoId != null &&
+            this.loginId != null
+        ) {
+            this.BloodTestFormData.UserId = this.userId;
+            this.BloodTestFormData.ResidentAdmissionInfoId =
+                this.residentAdmissionInfoId;
+            this.BloodTestFormData.StartedBy =
+                this.loginId;
+            this.BloodTestFormData.ModifiedBy =
+                this.loginId;
+
+            const objectBody: any = {
+                StatementType: this.StatementType,
+                BloodTestForm:
+                    this.BloodTestFormData,
+            };
+
+            this._UtilityService.showSpinner();
+            this.unsubscribe.add = this._FormService
+                .AddInsertUpdateFormData(objectBody)
+                .subscribe({
+                    next: (data) => {
+                        this._UtilityService.hideSpinner();
+                        if (data.actionResult.success == true) {
+                            this.EmitUpdateForm.emit(true);
+                            this._UtilityService.showSuccessAlert(
+                                data.actionResult.errMsg
+                            );
+                        } else
+                            this._UtilityService.showWarningAlert(
+                                data.actionResult.errMsg
+                            );
+                    },
+                    error: (e) => {
+                        this._UtilityService.hideSpinner();
+                        this._UtilityService.showErrorAlert(e.message);
+                    },
+                });
+        } else {
+            this._UtilityService.showWarningAlert(
+                'Resident admission details are missing.'
+            );
+        }
+    }
+
+    ResetModel() {
+        this.preSelectedFormData = <any>{};
+        this.isEditable = true;
+        this.BloodTestFormData = <any>{};
+        this.StatementType = 'Insert';
+    }
 }
