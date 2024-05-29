bb42e8ba1c9df8abc0b15e5317b28ee742ee9dd2
anushalav bhatt <161699666+anushalav09@users.noreply.github.com>
2024-05-29 12:24:00 +0530

    Created the Activities Chart Component along with Common Dropdown API for Charts in UserService File

---
+++ src/app/ui/components/charts/activities-chart/activity-chart.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ActivityChartService {
+
+  constructor(private _httpclient: HttpClient) { }
+
+
+  //#region ActivityChart
+
+  AddInsertUpdateActivityChartForm(ActivityChartFormData: any): Observable<any> {
+    let reqHeader = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': environment.BaseUriAdmin,
+      'Authorization': 'Bearer ' + localStorage.getItem('token')
+    });
+    let params = new HttpParams();
+    var data = JSON.stringify(ActivityChartFormData).toString();
+    console.log(data);
+    return this._httpclient.post<any>(
+      environment.BaseUriUser +
+      'api/User/AddInsertUpdateActivityChart',
+      data,
+      { headers: reqHeader, params: params }
+    );
+  }
+
+  GetActivityChartById(admissionid, userid): Observable<any> {
+    let reqHeader = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': environment.BaseUriAdmin,
+      'Authorization': 'Bearer ' + localStorage.getItem('token')
+    });
+    let params = new HttpParams();
+    params = params.append('admissionid', admissionid);
+    params = params.append('userid', userid);
+    return this._httpclient.get<any>(
+      environment.BaseUriUser + 'api/User/GetActivityChartById',
+      { headers: reqHeader, params: params }
+    );
+  }
+
+  //#endregion
+}
