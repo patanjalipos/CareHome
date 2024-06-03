03a5b444ec9775b503a2953f7bc36093bcc741db
Ishaan Kr. <ishaan.kumar@bharuwasolutions.com>
2024-06-03 17:21:12 +0530

    1. Resident API Updated 2. Form and Charts UI Designed and Implement (Need to fix)

--- src/app/ui/components/documentation/forms-and-charts/forms-and-charts.module.ts
+++ src/app/ui/components/documentation/forms-and-charts/forms-and-charts.module.ts
@@ -3,15 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { FormsAndChartsRoutingModule } from './forms-and-charts-routing.module';
 import { FormsAndChartsComponent } from './forms-and-charts.component';
-
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { FormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsDashboardModule } from '../../forms/forms-dashboard/forms-dashboard.module';
 
 @NgModule({
-  declarations: [
-    FormsAndChartsComponent
-  ],
-  imports: [
-    CommonModule,
-    FormsAndChartsRoutingModule
-  ]
+    declarations: [FormsAndChartsComponent],
+    imports: [
+        CommonModule,
+        FormsAndChartsRoutingModule,
+        FormsModule,
+        RadioButtonModule,
+        DropdownModule,
+        FormsDashboardModule
+    ],
 })
-export class FormsAndChartsModule { }
+export class FormsAndChartsModule {}
