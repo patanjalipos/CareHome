d1301047427e5a3f8139f1a2dbad30e4889c8038
anushalav bhatt <161699666+anushalav09@users.noreply.github.com>
2024-07-17 08:24:56 +0530

    create complete UI of must chart

--- src/app/ui/components/charts/must-chart/must-chart.module.ts
+++ src/app/ui/components/charts/must-chart/must-chart.module.ts
@@ -3,6 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { MustChartRoutingModule } from './must-chart-routing.module';
 import { MustChartComponent } from './must-chart.component';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CarouselModule } from 'primeng/carousel';
+import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { StrikeThroughEntryModule } from '../strike-through-entry/strike-through-entry.module';
+import { CalendarModule } from 'primeng/calendar';
 
 
 @NgModule({
@@ -11,7 +21,17 @@ import { MustChartComponent } from './must-chart.component';
   ],
   imports: [
     CommonModule,
-    MustChartRoutingModule
+    MustChartRoutingModule,
+    DropdownModule,
+    InputTextModule,
+    InputTextareaModule,
+    FormsModule,
+    CarouselModule,
+    SelectButtonModule,
+    StrikeThroughEntryModule,
+    DialogModule,
+    ButtonModule,
+    CalendarModule
   ],
   exports: [MustChartComponent]
 })
--- src/app/ui/components/charts/must-chart/must-chart.service.ts
+++ src/app/ui/components/charts/must-chart/must-chart.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MustChartService {
 
-  constructor() { }
+  constructor(private _httpclient: HttpClient) { }
+
+
+  //#region mustchart
+
+  AddInsertUpdatebloodGlucoseChartForm(bloodGlucoseChartFormData: any): Observable<any> {
+    let reqHeader = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': environment.BaseUriUser,
+      'Authorization': 'Bearer ' + localStorage.getItem('token')
+    });
+    let params = new HttpParams();
+    var data = JSON.stringify(bloodGlucoseChartFormData).toString();
+    return this._httpclient.post<any>(
+      environment.BaseUriUser +
+      'api/User/AddInsertUpdatebloodGlucoseChartForm',
+      data,
+      { headers: reqHeader, params: params }
+    );
+  }
+
 }
