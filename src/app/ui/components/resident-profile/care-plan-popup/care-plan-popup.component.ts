452a38686832e81baa8ab22ce382fbdcff2e48c9
anushalav bhatt <161699666+anushalav09@users.noreply.github.com>
2024-09-18 11:58:23 +0530

    complete UI of care plan

---
+++ src/app/ui/components/resident-profile/care-plan-popup/care-plan-popup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-care-plan-popup',
+  templateUrl: './care-plan-popup.component.html',
+  styleUrls: ['./care-plan-popup.component.scss']
+})
+export class CarePlanPopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
